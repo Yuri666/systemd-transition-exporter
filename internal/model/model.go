4e755602532e101b0ce22f8a325f468aa4162905
Yuri666 <yuri68@yandex.ru>
2026-08-22 19:24:56 +0300

    feat: add stable string names for transition event sources

--- internal/model/model.go
+++ internal/model/model.go
@@ -3,78 +3,56 @@ package model
 import "time"
 
 type AvailabilityState uint8
-
 const (
 	StateUnknown AvailabilityState = iota
 	StateUp
 	StateDown
 )
-
-func (s AvailabilityState) String() string {
-	switch s {
-	case StateUp:
-		return "up"
-	case StateDown:
-		return "down"
-	default:
-		return "unknown"
-	}
-}
+func (s AvailabilityState) String() string { switch s { case StateUp: return "up"; case StateDown: return "down"; default: return "unknown" } }
 
 type EventSource uint8
-
 const (
 	SourceSystemd EventSource = iota
 	SourceHostReboot
 	SourceRecovery
 )
+func (s EventSource) String() string { switch s { case SourceSystemd: return "systemd"; case SourceHostReboot: return "host_reboot"; case SourceRecovery: return "recovery"; default: return "unknown" } }
 
 type UnitSnapshot struct {
 	Service string
-
 	ActiveState string
-	SubState    string
-
+	SubState string
 	ActiveEnterTimestampUS uint64
-	ActiveExitTimestampUS  uint64
-
+	ActiveExitTimestampUS uint64
 	ActiveEnterTimestampMonotonicUS uint64
-	ActiveExitTimestampMonotonicUS  uint64
-
+	ActiveExitTimestampMonotonicUS uint64
 	BootID string
-
 	ObservedAt time.Time
 }
 
 type Event struct {
 	Sequence uint64
-
 	Service string
-	State   AvailabilityState
-
-	EventTimeUnixMS      int64
+	State AvailabilityState
+	EventTimeUnixMS int64
 	EventTimeMonotonicUS uint64
-	BootID               string
-	Source               EventSource
-
+	BootID string
+	Source EventSource
 	SystemdActiveState string
-	SystemdSubState    string
+	SystemdSubState string
 }
 
 type ServiceState struct {
 	Service string
-
 	Availability AvailabilityState
-	ActiveState  string
-	SubState     string
-	BootID       string
-
+	ActiveState string
+	SubState string
+	BootID string
 	LastActiveEnterTimestampUS uint64
-	LastActiveExitTimestampUS  uint64
+	LastActiveExitTimestampUS uint64
 	LastActiveEnterMonotonicUS uint64
-	LastActiveExitMonotonicUS  uint64
-
+	LastActiveExitMonotonicUS uint64
 	LastEventTimeUnixMS int64
-	LastEventMonoUS     uint64
-	LastSequence        uint64
+	LastEventMonoUS uint64
+	LastSequence uint64
 }
